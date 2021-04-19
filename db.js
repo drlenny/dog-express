cd9f0b4988b998cd7e3b4674c60e3369ae200e68
drlenny <66442932+drlenny@users.noreply.github.com>
2021-04-19 15:12:13 -0500

    update with part 2

--- db.js
+++ db.js
@@ -1,30 +1,30 @@
 module.exports = [
     {
-        breed: 'Doberman Pinscher',
+        breed: 'doberman',
         name: 'Boomer',
         image: 'https://www.petfirst.com/wp-content/uploads/2018/03/Breed-Hero-Doberman-1200x1200.jpg',
         goodBoy: 'Yes'
     },
     {
-        breed: 'Dalmation',
+        breed: 'dalmation',
         name: 'Pierre',
         image: 'https://www.pdsa.org.uk/media/7888/dalmatian-gallery-outdoors-8-min.jpg?anchor=center&mode=crop&quality=100&height=500&bgcolor=fff&rnd=132179483010000000',
         goodBoy: 'Yes'
     },
     {
-        breed: 'Malinoise',
+        breed: 'malinoise',
         name: 'Major',
         image: 'https://www.americangrit.com/wp-content/uploads/2019/11/Belgian_Malinois_Male_Puppy.jpg',
         goodBoy: 'Yes'
     },
     {
-        breed: 'Chihuahua',
+        breed: 'chihuahua',
         name: 'Tony',
         image: 'https://www.thegoodypet.com/wp-content/uploads/elementor/thumbs/what-is-a-deer-head-chihuahua-1-p3gvsvj23oe4ya3lyttvq9g3ykzh8bcgsjzd97r5ww.jpg',
         goodBoy: 'Yes'
     },
     {
-        breed: 'Basenji',
+        breed: 'basenji',
         name: 'Felix',
         image: 'https://i0.wp.com/puppytoob.com/wp-content/uploads/2015/02/basenji.jpg?resize=640%2C535',
         goodBoy: 'Yes'
--- index.js
+++ index.js
@@ -9,7 +9,23 @@ app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
 
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+const fetch = require('node-fetch');
+
 const server = http.createServer(app);
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+const helmet = require('helmet');
+app.use(helmet());
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     console.log(req.url);
@@ -38,15 +54,22 @@ app.get('/dogs', (req, res) => {
     // res.send("Testing Dogs")
 });
 
-app.get('/dogs/:breed', (req, res) => {
+app.get('/dogs/:breed', async (req, res) => {
     console.log(req.params.breed);
-    var {breed} = req.params;
-    var dog = db.find(thisDog => thisDog.breed === breed);
+    var { breed } = req.params;
+    var dog = db.find((thisDog) => thisDog.breed === breed);
 
-    if (dog){
+    if (dog) {
         console.log(dog);
 
-        res.render('dog', {
+        var pic = await fetch(`https://dog.ceo/api/breed/${dog.breed}/images/random`)
+            .then(res => res.json());
+
+        console.log(pic.message);
+
+        dog.image = pic.message
+
+        res.render('dog.html', {
             locals: {
                 dog,
                 title: "Dog",
@@ -56,7 +79,11 @@ app.get('/dogs/:breed', (req, res) => {
                 image: '/partials/image'
             }
         })
+    } else {
+        res.status(404)
+            .send(`No dog found`)
     }
+
 })
 
 
